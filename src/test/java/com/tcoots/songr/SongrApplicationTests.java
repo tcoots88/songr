d6c041e9182bd00c6763b4232965c3234435730c
tcoots88 <travis.coots@yahoo.com>
2020-01-21 10:52:19 -0800

    initial commit from Spring init

---
+++ src/test/java/com/tcoots/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcoots.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
